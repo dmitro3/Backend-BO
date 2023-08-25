1b6c1c0d2948a363673b7f2588b427150afaa677
Dung Dam <dungdamquoc1993@gmail.com>
2023-08-25 12:30:09 +0700

    before edit

---
+++ testBtc.js
@@ -0,0 +1,148 @@
+const { PrivateKey } = require("bitcore-lib");
+
+const { mainnet, testnet } = require("bitcore-lib/lib/networks");
+const Mnemonic = require("bitcore-mnemonic");
+
+const createWallet = (network = testnet) => {
+  var privateKey = new PrivateKey();
+  var address = privateKey.toAddress(network);
+  return {
+    privateKey: privateKey.toString(),
+    address: address.toString(),
+  };
+};
+
+const createHDWallet = (network = testnet) => {
+  let passPhrase = new Mnemonic(Mnemonic.Words.SPANISH);
+  let xpriv = passPhrase.toHDPrivateKey(passPhrase.toString(), network);
+
+  return {
+    xpub: xpriv.xpubkey,
+    privateKey: xpriv.privateKey.toString(),
+    address: xpriv.publicKey.toAddress().toString(),
+    mnemonic: passPhrase.toString(),    
+  };
+};
+
+const axios = require("axios");
+const bitcore = require("bitcore-lib");
+const TESTNET = true;
+
+const sendBitcoin = async (sendWallet, recieverWallet, amountToSend) => { // chuyển tiền và tiền thừa trong utxo về ví người nhận
+  try {
+    const satoshiToSend = Math.round(amountToSend * 100000000);
+    let fee = 0;
+    let inputCount = 0;
+    let outputCount = 2;
+    debugger;
+    // const recommendedFee = await axios.get(
+    //   "https://bitcoinfees.earn.com/api/v1/fees/recommended"
+    // );
+
+    const transaction = new bitcore.Transaction();
+    let totalAmountAvailable = 0;
+
+    let inputs = [];
+
+    const resp = await axios({
+      method: "GET",
+      url: `https://blockstream.info/testnet/api/address/${sendWallet.address}/utxo`,
+    });
+    const utxos = resp.data;
+
+    for (const utxo of utxos) {
+      let input = {};
+      input.satoshis = utxo.value;
+      input.script = bitcore.Script.buildPublicKeyHashOut(
+        sendWallet.address
+      ).toHex();
+      input.address = sendWallet.address;
+      input.txId = utxo.txid;
+      input.outputIndex = utxo.vout;
+      totalAmountAvailable += utxo.value;
+      inputCount += 1;
+      inputs.push(input);
+    }
+
+    /**
+     * In a bitcoin transaction, the inputs contribute 180 bytes each to the transaction,
+     * while the output contributes 34 bytes each to the transaction. Then there is an extra 10 bytes you add or subtract
+     * from the transaction as well.
+     * */
+    const transactionSize =
+      inputCount * 180 + outputCount * 34 + 10 - inputCount;
+
+    // fee = (transactionSize * recommendedFee.data.hourFee) / 3; // satoshi per byte
+    if (TESTNET) {
+      fee = transactionSize * 1; // 1 sat/byte is fine for testnet
+    }
+    debugger;
+    if (totalAmountAvailable - satoshiToSend - fee < 0) {
+      throw new Error("Balance is too low for this transaction");
+    }
+    //Set transaction input
+    transaction.from(inputs);
+
+    // set the recieving address and the amount to send
+    transaction.to(recieverWallet.address, satoshiToSend);
+
+    // Set change address - Address to receive the left over funds after transfer
+    transaction.change(recieverWallet.address);
+
+    //manually set transaction fees: 20 satoshis per byte
+    transaction.fee(Math.round(fee));
+
+    // Sign transaction with your private key
+    transaction.sign(sendWallet.privateKey);
+
+    // serialize Transactions
+    const serializedTransaction = transaction.serialize();
+    debugger;
+    // Send transaction
+    const result = await axios({
+      method: "POST",
+      url: `https://blockstream.info/testnet/api/tx`,
+      data: serializedTransaction,
+    });
+    return result.data;
+  } catch (error) {
+    debugger;
+    return error;
+  }
+};
+const getBTCutxos = async (theWallet) => {
+  const resp = await axios({
+    method: "GET",
+    url: `https://blockstream.info/testnet/api/address/${theWallet.address}/utxo`,
+  });
+  console.log(resp.data);
+};
+
+const wallet = {
+  privateKey:
+    "aa5b18308f8576d4520559649d95c1cd7ab4d22e48415cb0d891ad78cf296dd2",
+  address: "myNeUWabCGwNHsLFfof3CJyCigmRpWYAiN",
+};
+const hdWallet = {
+  xpub: "tpubD6NzVbkrYhZ4XeunZEo12ffFs67LXszDEqG1Rjn7ehJQznzgd99pbszmyHXtRApDHSHo2DeGfswBUxwiRvCrKaUX3KVAhxeXLzKx6CxinKX",
+  privateKey:
+    "a6cd0dad540944788794e325798dfa9f39baf1085682510e08611fb47037d62f",
+  address: "mgiQkxMZ2y9yYSTQRQEjs1UCpkYUMsyNi2",
+  mnemonic:
+    "embudo letal válido aporte sopa bucle flúor núcleo regla legión viejo equipo",
+};
+
+
+// sendBitcoin(hdWallet, wallet, 0.002);
+
+//   {
+//     xpub: 'tpubD6NzVbkrYhZ4XeunZEo12ffFs67LXszDEqG1Rjn7ehJQznzgd99pbszmyHXtRApDHSHo2DeGfswBUxwiRvCrKaUX3KVAhxeXLzKx6CxinKX',
+//     privateKey: 'a6cd0dad540944788794e325798dfa9f39baf1085682510e08611fb47037d62f',
+//     address: 'mgiQkxMZ2y9yYSTQRQEjs1UCpkYUMsyNi2',
+//     mnemonic: 'embudo letal válido aporte sopa bucle flúor núcleo regla legión viejo equipo'
+//   }
+//   Mac-Pro:backend apple$ node testBtc.js
+//   {
+//     privateKey: 'aa5b18308f8576d4520559649d95c1cd7ab4d22e48415cb0d891ad78cf296dd2',
+//     address: 'myNeUWabCGwNHsLFfof3CJyCigmRpWYAiN'
+//   }
---
+++ testEth.js
@@ -0,0 +1,108 @@
+const Web3 = require("web3")
+const axios = require('axios')
+
+const web3 = new Web3(
+  new Web3.providers.WebsocketProvider(
+    "wss://bsc.getblock.io/d467b2c6-6450-4b3e-a670-0c7deb4f2a2a/testnet/"
+  )
+);
+require("dotenv").config();
+
+const bscScanApi = process.env.BSC_SCAN_API
+const encryptPassword = process.env.ETH_ENCRYPT_PASSWORD;
+
+const wallet = web3.eth.accounts.create();
+const firstEncrypt = wallet.encrypt(encryptPassword);
+const secondEncrypt = web3.eth.accounts.encrypt(
+  wallet.privateKey,
+  encryptPassword
+);
+
+const firstDecrypt = web3.eth.accounts.decrypt(firstEncrypt, encryptPassword);
+const secondDecrypt = web3.eth.accounts.decrypt(secondEncrypt, encryptPassword);
+// console.log("firstEncrypt: ", firstEncrypt);
+// console.log("secondEncrypt: ", secondEncrypt);
+// console.log("wallet: ", wallet);
+// console.log("firstDecrypt: ", firstDecrypt);
+// console.log("secondDecrypt: ", secondDecrypt);
+
+async function checkBalance(address) {
+  // phục vụ cho user gửi tiền
+  try {
+    const balanceWei = await web3.eth.getBalance(address);
+    const balanceNativeToken = web3.utils.fromWei(balanceWei, "ether");
+
+    console.log(`Balance of ${address}: ${balanceNativeToken} ETH`);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+async function sendAndTrackTransaction(senderPrivateKey, receiverAddress) {
+  try {
+    const senderAccount =
+      web3.eth.accounts.privateKeyToAccount(senderPrivateKey);
+
+    const nonce = await web3.eth.getTransactionCount(senderAccount.address);
+    const gasPrice = await web3.eth.getGasPrice();
+
+    const balanceWei = await web3.eth.getBalance(senderAccount.address);
+
+    const gasLimit = 21000; // Số gas cố định cho việc gửi ETH/BNB
+
+    const txObject = {
+      nonce: web3.utils.toHex(nonce),
+      gasPrice: web3.utils.toHex(gasPrice),
+      to: receiverAddress,
+      value: web3.utils.toHex(balanceWei - gasPrice * gasLimit), // Trừ phí gas
+      gas: web3.utils.toHex(gasLimit),
+    };
+
+    const signedTx = await senderAccount.signTransaction(txObject);
+
+    const tx = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction
+    );
+    if (tx.transactionHash) {
+        const apiUrl = `https://api-testnet.bscscan.com/api?module=transaction&action=getstatus&txhash=${tx.transactionHash}&apikey=${bscScanApi}`
+        const checkBscScan = await axios.get(apiUrl)
+        const status = checkBscScan.data.result.isError === '0' ? 'Success' : 'Fail';
+        if(status == 'Success') {
+            console.log('check bsc scan ok')
+        } else {
+            console.log('check bsc scan lỗi') 
+        }
+    } else {
+        console.log('check bsc scan lỗi')
+    }
+  } catch (error) {
+    console.error("Bo Me Roi:", error);
+  }
+}
+
+const senderPrivateKey = "0x8a07d2d230dcb97272576b6a4f8841f06cf4b865a9d6e241925287421bd57b00"; // Thay bằng private key của ví gửi
+const receiverAddress = "0x59772e95C77Dd1575fB916DACDFabEF688cc7971"; // Thay bằng địa chỉ ví nhận
+
+sendAndTrackTransaction(senderPrivateKey, receiverAddress);
+
+// {
+//     version: 3,
+//     id: '409ff035-f32f-4ad4-8b35-b8600c436778',
+//     address: '9a7ed79be424e40aa965729e2b94cb5b78e8bce3',
+//     crypto: {
+//       ciphertext: 'beb145739ece1715fa3af2e190f784ab381856caf890bad75d9a947c650d47d6',
+//       cipherparams: { iv: '27e1a24f78ff7ccb4555e43cd67e9325' },
+//       cipher: 'aes-128-ctr',
+//       kdf: 'scrypt',
+//       kdfparams: {
+//         dklen: 32,
+//         salt: '6a84697eb2090e1a873c85429f64972e00388cb953ad588a2355e4c3cb07744a',
+//         n: 8192,
+//         r: 8,
+//         p: 1
+//       },
+//       mac: '528395a61d39da7c73a50527f9f997d7ae976fbdb0c4c5a2356a1182e25ec8bf'
+//     }
+//   }
+// address: '0x9a7ed79Be424E40AA965729E2B94cb5B78E8BCe3',
+// privateKey: '0x8a07d2d230dcb97272576b6a4f8841f06cf4b865a9d6e241925287421bd57b00',
